996c37141d53b29845bb1c7d92f8cb68772b6353
Pavlovsky Louis <louis.pavlovsky@epitech.eu>
2021-03-22 18:56:52 +0100

    [ADD] : collision square/square and square/center add of collision btw 2 square and btw a square and center, all in the folders collision

--- src/scen/default_page.c
+++ src/scen/default_page.c
@@ -8,10 +8,17 @@
 #include "window_struc.h"
 #include "window_fonction.h"
 
+int check_if_collision_btw_square_without_rotation(sfSprite *const sprite_1\
+, sfSprite *const sprite_2);
+
+sfCircleShape *init_hitbox_circle(int const radius, sfSprite *sprite);
+
+int check_if_collision_btw_circle_square(int const radius\
+, sfSprite *const sprite_circle, sfSprite *const sprite_square);
+
 static void draw(the_window *windows)
 {
     sfRenderWindow_clear(windows->window, sfBlack);
-    sfRenderWindow_display(windows->window);
 }
 
 static void update(the_window *windows)
@@ -21,9 +28,10 @@ static void update(the_window *windows)
 
 void default_page(the_window *windows)
 {
-    while (sfRenderWindow_isOpen(windows->window)) {
+   while (sfRenderWindow_isOpen(windows->window)) {
         speed_of_game((float)1/60);
         update(windows);
+        sfRenderWindow_display(windows->window);
         while (sfRenderWindow_pollEvent(windows->window, &windows->event)) {
             if (windows->event.type == sfEvtClosed)
                 sfRenderWindow_close(windows->window);
