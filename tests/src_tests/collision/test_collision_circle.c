d27b1eb782cb8625e02ab0e03cc1016b093984dd
Pavlovsky Louis <louis.pavlovsky@epitech.eu>
2021-03-25 21:58:40 +0100

    [FIX] : Norme norme of the criterion tests

--- tests/src_tests/collision/test_collision_circle.c
+++ tests/src_tests/collision/test_collision_circle.c
@@ -2,13 +2,6 @@
 ** EPITECH PROJECT, 2021
 ** B-MUL-200-BDX-2-1-myrpg-louis.pavlovsky
 ** File description:
-** test_collision_circle
-*/
-
-/*
-** EPITECH PROJECT, 2021
-** B-MUL-200-BDX-2-1-myrpg-louis.pavlovsky
-** File description:
 ** test_collision_square
 */
 
