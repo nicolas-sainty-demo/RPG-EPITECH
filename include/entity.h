c955d68d51da404b59c8f83079583423be6bab3c
truchejude <jude.marzat@epitech.eu>
2021-04-28 11:30:11 +0200

    [ADD] enemy no ai

--- include/entity.h
+++ include/entity.h
@@ -9,17 +9,33 @@
 #define ENTITY_H_
 
 #include <SFML/Graphics/Sprite.h>
+#include <SFML/System/Clock.h>
 
 typedef struct entity_passive
 {
     sfSprite *sprite;
     char anime;
+    sfClock *animation_clock;
+    sfBool flip;
+    int type;
+    int hp;
+    int speed;
+    int damage;
 } entity_passive_t;
 
 typedef struct entity_enemy
 {
     sfSprite *sprite;
     char anime;
+    int type;
+    int hp;
+    int speed;
+    int damage;
+    sfClock *animation_clock;
+    sfBool flip;
 } entity_enemy_t;
+
+void anim_enemy(entity_enemy_t *enemy);
+void anim_passive(entity_passive_t *passive);
 
 #endif /* !ENTITY_H_ */
