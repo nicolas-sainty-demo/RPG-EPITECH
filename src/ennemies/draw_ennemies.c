69669a9e32b24a9a70128d8dd6b675468bf07e7f
Pavlovsky Louis <louis.pavlovsky@epitech.eu>
2021-05-04 09:42:22 +0200

    [ADD] : activation of flip

--- src/ennemies/draw_ennemies.c
+++ src/ennemies/draw_ennemies.c
@@ -15,6 +15,10 @@ void draw_ennemies(the_window *windows)
         sfRenderWindow_drawSprite\
         (windows->window, windows->scene->enemy[i]->sprite, NULL);
         anim_enemy(windows->scene->enemy[i]);
+        if (windows->scene->enemy[i]->speed_vector.x >= 0) {
+            windows->scene->enemy[i]->flip = sfFalse;
+        } else
+            windows->scene->enemy[i]->flip = sfTrue;
         if (windows->scene->enemy[i]->hp != \
         windows->scene->enemy[i]->hp_max && windows->scene->enemy[i]->hp > 0)
             show_healbar(windows, i);
