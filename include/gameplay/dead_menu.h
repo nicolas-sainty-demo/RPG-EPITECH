beb3158a31bf9be9ec58050bdf39ece8938af638
theo abitbol <theo@tetew.home>
2021-05-05 04:53:34 -0400

    [ADD] new_branch dead menu

---
+++ include/gameplay/dead_menu.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2021
+** B-MUL-200-BDX-2-1-myrpg-louis.pavlovsky
+** File description:
+** dead_menu
+*/
+
+#ifndef DEAD_MENU_H_
+#define DEAD_MENU_H_
+
+
+#endif /* !DEAD_MENU_H_ */
